52d67a2bfdf340c63dc8da53f93159e96a731c16
Muhammad Awais Abid Hussain <kingyt6624@gmail.com>
2025-02-05 22:15:36 +0500

    Create form for adding task with description

--- todo-app/app/page.js
+++ todo-app/app/page.js
@@ -1,3 +1,4 @@
+"use client";
 import React from "react";
 
 const page = () => {
@@ -6,6 +7,21 @@ const page = () => {
       <h1 className="bg-black h-20 text-center py-4 tracking-widest text-4xl font-bold text-cyan-50">
         Todo List
       </h1>
+      <form action="">
+        <input
+          className="border-b-slate-950 m-7 px-3 py-2 border-2 text-2xl"
+          type="text"
+          placeholder="Add a new task"
+        />
+        <input
+          className="border-b-slate-950 m-7 px-3 py-2 border-2 text-2xl"
+          type="text"
+          placeholder="Enter the description"
+        />
+        <button className="px-7 py-3 text-xl font-semibold rounded-sm bg-green-800 text-white ">
+          Add Task
+        </button>
+      </form>
     </>
   );
 };
